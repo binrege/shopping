04675d9c2a2fcac1dea6181eea7161bc6d9e3fd9
yangyupang <1156619927@qq.com>
2019-09-26 16:22:15 +0800

    开始个人中心

---
+++ src/views/personal/Personal.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="all">
+    <div class="head">
+    <div class="head-person">
+
+    </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style  scoped>
+.all{
+  width: 100%;
+  height: 100%;
+}
+.head{
+width: 100%;
+background:#ac0215;
+position:absolute;
+height: 30%;
+}
+.head-person{
+width: 80%;
+background:#f5f1f1;
+position:relative;
+height: 90%;
+
+margin: 50px auto;
+}
+</style>
