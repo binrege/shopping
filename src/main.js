5c74cf12c39a23573b1d57065ef7b3cd5978308f
binrege <752228158@qq.com>
2019-10-09 10:47:16 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -18,7 +18,9 @@ import { Search } from 'vant';
 import { Tag } from 'vant';
 import { Dialog } from 'vant';
 import { DropdownMenu, DropdownItem } from 'vant';
-import './mock'
+import './mock';
+import 'vant/lib/index.css';
+import { Swipe, SwipeItem } from 'vant';
 Vue.use(DropdownMenu).use(DropdownItem);
 Vue.use(Tag);
 Vue.use(Search);
@@ -29,8 +31,6 @@ Vue.use(Dialog);
 Vue.prototype.$axios=axios;//将axios挂载在整个vue上，Vue是整个根实例，然后若想发送ajax请求，只需要this.$axios.路径
 Vue.prototype.$dayjs=dayjs;
 Vue.config.productionTip = false;
-import 'vant/lib/index.css';
-import { Swipe, SwipeItem } from 'vant';
 Vue.use(Swipe).use(SwipeItem);
 Vue.use(VueLazyload)
 // or with options
@@ -53,9 +53,5 @@ new Vue({
     Subscribe,
     Commodity,
     Settlement,
-
-
-
-
     render: h => h(App)
 }).$mount('#app')
